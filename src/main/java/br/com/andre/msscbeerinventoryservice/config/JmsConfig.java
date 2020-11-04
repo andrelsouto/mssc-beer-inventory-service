17b9458bdb4aaf5c961f3710c0ffb5e145b363c5
André Luiz <andre.andrade@vsoft.com.br>
2020-11-04 08:31:59 -0300

    Adding JMS config.

---
+++ src/main/java/br/com/andre/msscbeerinventoryservice/config/JmsConfig.java
@@ -0,0 +1,20 @@
+package br.com.andre.msscbeerinventoryservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter jacksonJmsConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+}
